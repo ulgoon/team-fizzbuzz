165f37222d3dcc7857d1e96d582d6eb9b9481b28
ulgoon <ulgoon89@gmail.com>
2021-11-25 17:44:34 +0900

    feat: Run do_fizzbuzz only __main__

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
-for i in range(1,16+1):
-    if i%3==0 or i%5==0:
-        print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
-    else:
-        print(i)
+def do_fizzbuzz():
+    for i in range(1,16+1):
+        if i%3==0 or i%5==0:
+            print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
+        else:
+            print(i)
+
+if __name__=='__main__':
+    do_fizzbuzz()
